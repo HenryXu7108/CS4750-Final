3a17595b140676f351936f34089d9d73a09a2e38
Xin <xs7tng@virginia.edu>
2023-12-02 14:24:26 -0500

    minor change: no effect

--- dashboard.php
+++ dashboard.php
@@ -1,14 +1,12 @@
 <?php
 session_start();
 
-// Check if the user is logged in, if not then redirect to login page
 if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
     header("Location: login.html");
     exit;
 }
 
 echo "Welcome " . $_SESSION['name'] . "!";
-// Log out link
 echo "<p><a href='logout.php'>Logout</a></p>";
 
 ?>
