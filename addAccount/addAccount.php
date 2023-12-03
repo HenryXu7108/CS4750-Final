5fc94cb7fa4a0882839a5fa5ee13b8fc7b6b5bfe
Xin <xs7tng@virginia.edu>
2023-12-02 19:31:36 -0500

    feat: add update_accounts_balance functionality

--- addAccount/addAccount.php
+++ addAccount/addAccount.php
@@ -4,8 +4,43 @@
     <meta charset="UTF-8">
     <title>Add an Account</title>
     <link rel="stylesheet" href="styles.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="icon" type="image/png" href="http://www.cs.virginia.edu/~up3f/cs4750/images/db-icon.png" />
+
 </head>
 <body>
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed top" aria-label="Main Navigation Bar">
+        <div>
+            <a class="navbar-brand" href="../dashboard.php" style="padding-left: 20px; text-align: center">Financial Tracker</a>
+        </div>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" >
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" style="justify-content: right" id="navbarCollapse">
+            <ul class="navbar-nav mb-2 mb-md-0">
+                <li class="nav-item">
+                    <a class="nav-link " aria-current="page" href="../dashboard.php">DashBoard</a>
+                </li>
+
+                <li class="nav-item">
+                    <a href="../insert.php" class="nav-link ">Insert Records</a>
+                </li>
+
+                <li class="nav-item">
+                    <a href="../addBillNotification/addBillNotification.php" class="nav-link">Add BillNotification</a>
+                </li>
+
+                <li class="nav-item">
+                    <a href="main.html" class="nav-link ">View Records</a>
+                </li>
+
+                <li class="nav-item">
+                    <a href="logout.php" class="nav-link ">Log Out</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
     <h1>Add an Account</h1>
     <form id="AccountForm" action="submit_addAccount.php" method="post">  
         <label for="accountBranchName">Branch Name:</label>
--- insert.php
+++ insert.php
@@ -1,9 +1,25 @@
+<?php 
+session_start();
+$person_id = $_SESSION['person_id'];
+require_once('connect-db.php');
+
+$stmt = $db->prepare("SELECT account_id, account_number, account_type FROM Account WHERE person_id = :person_id");
+$stmt->bindParam(':person_id', $person_id, PDO::PARAM_INT);
+$stmt->execute();
+
+$accounts = $stmt->fetchAll(PDO::FETCH_ASSOC);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Add a Transaction</title>
     <link rel="stylesheet" href="styles.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="icon" type="image/png" href="http://www.cs.virginia.edu/~up3f/cs4750/images/db-icon.png" />
+
     <style>
         .fields, #submitBtn {
             display: none;
@@ -11,6 +27,38 @@
     </style>
 </head>
 <body>
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed top" aria-label="Main Navigation Bar">
+        <div>
+            <a class="navbar-brand" href="dashboard.php" style="padding-left: 20px; text-align: center">Financial Tracker</a>
+        </div>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" >
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" style="justify-content: right" id="navbarCollapse">
+            <ul class="navbar-nav mb-2 mb-md-0">
+                <li class="nav-item">
+                    <a class="nav-link " aria-current="page" href="dashboard.php">DashBoard</a>
+                </li>
+
+                
+                <li class="nav-item">
+                    <a href="addAccount/addAccount.php" class="nav-link">Add Account</a>
+                </li>
+
+                <li class="nav-item">
+                    <a href="addBillNotification/addBillNotification.php" class="nav-link">Add BillNotification</a>
+                </li>
+
+                <li class="nav-item">
+                    <a href="main.html" class="nav-link ">View Records</a>
+                </li>
+
+                <li class="nav-item">
+                    <a href="logout.php" class="nav-link ">Log Out</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
     <h1>Add a Transaction</h1>
 
     <form id="transactionForm" action="submit_transaction.php" method="post">  
@@ -32,9 +80,12 @@
             </select>
             <label for="incomeAccount">Account:</label>
             <select name="incomeAccount" id="incomeAccount">
-                <option value="Saving">Saving Account</option>
-                <option value="Credit">Credit Card</option>
-            </select>
+                <?php foreach ($accounts as $account): ?>
+                    <option value="<?= htmlspecialchars($account['account_id']) ?>">
+                        <?= htmlspecialchars($account['account_number']) . " (" . htmlspecialchars($account['account_type']) . ")" ?>
+                    </option>
+                <?php endforeach; ?>
+             </select>
             <label for="incomeAmount">Amount:</label>
             <input type="number" name="incomeAmount">
             <label for="incomeDate">Date:</label>
@@ -55,9 +106,11 @@
             </select>
             <label for="expenseAccount">Account:</label>
             <select name="expenseAccount" id="expenseAccount">
-                <option value="Saving">Saving Account</option>
-                <option value="Checking">Checking Account</option>
-                <option value="Credit">Credit Account</option>
+                <?php foreach ($accounts as $account): ?>
+                    <option value="<?= htmlspecialchars($account['account_id']) ?>">
+                        <?= htmlspecialchars($account['account_number']) . " (" . htmlspecialchars($account['account_type']) . ")" ?>
+                    </option>
+                <?php endforeach; ?>
             </select>
             <label for="expenseAmount">Amount:</label>
             <input type="number" name="expenseAmount">
@@ -68,9 +121,11 @@
         <div id="investmentFields" class="fields">
             <label for="investmentAccount">Account:</label>
             <select name="investmentAccount" id="investmentAccount">
-                <option value="Saving">Saving Account</option>
-                <option value="Checking">Checking Account</option>
-                <option value="Credit">Credit Account</option>
+                <?php foreach ($accounts as $account): ?>
+                    <option value="<?= htmlspecialchars($account['account_id']) ?>">
+                        <?= htmlspecialchars($account['account_number']) . " (" . htmlspecialchars($account['account_type']) . ")" ?>
+                    </option>
+                <?php endforeach; ?>
             </select>
             <label for="investmentType">Investment Type:</label>
             <select name="investmentType" id="investmentType">
@@ -93,10 +148,31 @@
             <textarea name="investmentNotes" placeholder="Notes"></textarea>
         </div>
         <div id="transactionFields" class="fields">
-            <input type="text" name="transactionSourceAccount" placeholder="Source Account">
-            <input type="text" name="transactionDestinationAccount" placeholder="Destination Account">
-            <input type="text" name="transactionType" placeholder="Transaction Type">
+            <label for="transactionSourceAccount">Source Account:</label>
+            <select name="transactionSourceAccount" id="transactionSourceAccount">
+                <?php foreach ($accounts as $account): ?>
+                    <option value="<?= htmlspecialchars($account['account_id']) ?>">
+                        <?= htmlspecialchars($account['account_number']) . " (" . htmlspecialchars($account['account_type']) . ")" ?>
+                    </option>
+                <?php endforeach; ?>
+            </select>
+            <label for="transactionDestinationAccount">Destination Account:</label>
+            <select name="transactionDestinationAccount" id="transactionDestinationAccount">
+                <?php foreach ($accounts as $account): ?>
+                    <option value="<?= htmlspecialchars($account['account_id']) ?>">
+                        <?= htmlspecialchars($account['account_number']) . " (" . htmlspecialchars($account['account_type']) . ")" ?>
+                    </option>
+                <?php endforeach; ?>
+            </select>
+            <label for="transactionType">Transaction Type:</label>
+            <select name="transactionType" id="transactionType">
+                <option value="DEPOSIT">DEPOSIT</option>
+                <option value="TRANSFER">TRANSFER</option>
+                <option value="RETRIEVAL">RETRIEVAL</option>
+            </select>
+            <label for="transactionAmount">Transaction Amount</label>
             <input type="number" name="transactionAmount" placeholder="Amount">
+            <label for="transactionDate">Transaction Date</label>
             <input type="date" name="transactionDate">
             <textarea name="transactionNotes" placeholder="Notes"></textarea>
         </div>
